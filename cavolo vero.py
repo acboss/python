af960def372d6d17163d00927a2fde063b800267
CyborgTrooper <Alessandro.lelii@outlook.com>
2018-06-05 17:00:56 +0200

    4

--- cavolo vero.py
+++ cavolo vero.py
@@ -4,8 +4,9 @@ Soprannome = 'Cecca'
 Eta = 17
 p = 'PYTHON: '
 y = 'You: '
-print (p + 'Ciao, come ti chiami?')
+print (y + "Ciao, il mio nome e' Python e sono tipo dio, ma a differenza del vero dio io faccio tutto quello che mi dici di fare devi sapere solo coome parlarmi :) \nTu invece come ti chiami?")
 print (y + Nome)
-print (p + "Bene..., pero' " + Nome + ' non mi piace come nome posso chiamarti Gervaso?')
+print (p + "Bene..., pero' " + Nome + ' non mi piace come nome, posso chiamarti Gervaso?')
 print (y + "No ma che razza di nome e' Gervaso, piuttosto chiamami " + Soprannome)
 print (p + "Mi piace da oggi sarai il mio " + Soprannome + " personale")
+print (p + "Vabene")
