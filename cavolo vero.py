43735f45c4018f159c2c215a5204029c95f068dd
CyborgTrooper <Alessandro.lelii@outlook.com>
2018-06-05 16:47:09 +0200

    null

---
+++ cavolo vero.py
@@ -0,0 +1,11 @@
+Nome = 'Andrea'
+Cognome = 'Ceccarelli'
+Soprannome = 'Cecca'
+Eta = 17
+p = 'PYTHON: '
+y = 'You: '
+print (p + 'Ciao, come ti chiami?')
+print (y + Nome)
+print (p + "Bene..., pero' " + Nome + ' non mi piace come nome posso chiamarti Gervaso?')
+print (y + "No ma che razza di nome e' Gervaso, piuttosto chiamami " + Soprannome)
+print (p + "Mi piace da oggi sarai il mio " + Soprannome + " personale")
