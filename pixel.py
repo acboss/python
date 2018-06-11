633536dac6b9fc3e9dcf4c4491241bb654b0818d
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-11 11:04:45 +0200

    11

---
+++ pixel.py
@@ -0,0 +1,88 @@
+import turtle
+t = turtle.Turtle()
+def An(t):
+    t.pu()
+    t.lt(90)
+    t.pd()
+    t.fd(20)
+    t.pu()
+    t.fd(10)
+    t.pd()
+    t.fd(20)
+    t.pu()
+    t.fd(10)
+    t.rt(90)
+    t.fd(10)
+    t.pd()
+    t.fd(20)
+    t.pu()
+    t.fd(10)
+    t.rt(90)
+    t.fd(10)
+    t.pd()
+    t.fd(20)
+    t.pu()
+    t.fd(10)
+    t.pd()
+    t.fd(20)
+    t.pu()
+    t.rt(90)
+    t.fd(10)
+    t.rt(90)
+    t.fd(20)
+    t.lt(90)
+    t.pd()
+    t.fd(20)
+    t.pu()
+    t.bk(30)
+    t.lt(90)
+    t.fd(20)
+    t.lt(90)
+    t.fd(20)
+    t.pd()
+
+def Bn(t):
+    t.lt(90)
+    t.fd(60)
+    t.rt(90)
+    t.fd(20)
+    t.pu()
+    t.fd(10)
+    t.rt(90)
+    t.fd(10)
+    t.pd()
+    t.fd(10)
+    t.pu()
+    t.fd(10)
+    t.pd()
+    t.fd(20)
+    t.pu()
+    t.fd(10)
+    t.rt(90)
+    t.fd(10)
+    t.pd()
+    t.fd(20)
+
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+An(t)
+Bn(t)
+turtle.mainloop()
