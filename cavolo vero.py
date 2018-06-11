c9da951d5a64daf160d9d9ffe02dda8c0f0cdcca
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-11 12:32:47 +0200

    asd

--- cavolo vero.py
+++ cavolo vero.py
@@ -1,19 +1,17 @@
-Nome = "Andrea"
-Cognome = "Ceccarelli"
-Soprannome = "Cecca"
-Eta = 17
-Risposta = "No"
+
 p = "PYTHON: "
 y = "You: "
-print (p + "Ciao, il mio nome e' Python e sono tipo Dio, ma a differenza del vero Dio io faccio tutto quello che mi dici di fare devi sapere solo coome parlarmi :) \nTu invece come ti chiami?")
-print (y + Nome)
+print (p + "Ciao, il mio nome e' Python e sono tipo Dio, ma a differenza del vero Dio io faccio tutto quello che mi dici di fare devi sapere solo come parlarmi :) \n        Tu invece come ti chiami?")
+Nome = input(y)
 print (p + "Bene..., pero' " + Nome + ' non mi piace come nome, posso chiamarti Gervaso?')
+Risposta = input(y)
 if Risposta == "No" or Risposta == "no" :
-    print (y + "No ma che razza di nome e' Gervaso, piuttosto chiamami " + Soprannome)
+    print (y + "Ma che nome e' Gervaso, piuttosto chiamami " )
+    Soprannome = input('     ')
     print (p + "Mi piace da oggi sarai il mio " + Soprannome + " personale")
     print (y + "Vabene")
     print (p + "Perfetto " + Soprannome + ", ora cosa vuoi fare?")
 elif Risposta == "Si" or Risposta == "si" :
     print (p + "Perfetto Gervaso, ora cosa vuoi fare?")
 else:
-    print ("Risposta non valida, rispondi solo si o no")
+    print ("RISPOSTA NON VALIDA, RISPONDI SOLO SI O NO")
