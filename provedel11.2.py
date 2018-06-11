6d01a25c12e762949a783288e2bc1ec3074b8df6
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-11 16:21:26 +0200

    orologio

--- provedel11.2.py
+++ provedel11.2.py
@@ -1,15 +1,25 @@
 import time
-time.time()
-def giorni():
-    print (time.time()//24)
-def ore():
-    h = time.time()%24
-    priint (h//60)
-    return h
-def minuti():
-    h = ore()
-    min = (h%60)//60
-    print (min)
+tempo = time.time()
+
+def sec(t):
+    print('sec'+ str((t%60)*60))
+def minuti(t):
+    min = t//60
+    print('min'+ str(((min%60)*60)+min))
     return min
-def secondi():
-    min//
+def ore(t):
+    min = minuti(t)
+    h = min//60
+    print('min'+ str(((h%24)*60)+h))
+    return h
+def giorni(t):
+    h = ore(t)
+    print ('h'+ str(h//24))
+
+sec(tempo)
+minuti(tempo)
+ore(tempo)
+giorni(tempo)
+
+    
+
