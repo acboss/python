53b9a85ca1d674924306effaef4696c10a565158
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-11 12:06:40 +0200

    12.1

--- provedel11.py
+++ provedel11.py
@@ -13,12 +13,16 @@ def stampa_n(s, n):
     stampa_n(s, n-1)
 
 def fai_n(j,n):
-    if i in range (n):
-        j(s,n)
+    if n <= 0:
+        return
+    elif n>0:
+        j
+        fai_n(j,n-1)
+        
 #Come esercizio, disegnate il diagramma di stack della funzione stampa_n chiamata con
 #s=Ciao e n=2. Poi, scrivete una funzione di nome fai_n che accetti come argomenti un
 #oggetto funzione e un numero n, e che chiami per n volte la funzione data
 N = 2
-s = ciao
-fai_n(stampa_n,N)
-contoallarovescia
+S = 'ciao'
+stampa_n(S,N)
+fai_n(stampa_n(S,N),N)
