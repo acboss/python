6ccbe29593837c3c23d8a705114e50ec9ff72ecc
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-11 11:43:34 +0200

    12

--- provedel11.py
+++ provedel11.py
@@ -1,1 +1,24 @@
-print (45678945%10)
+def contoallarovescia(n):
+    if n <= 0:
+        print('Via!')
+    else:
+        print(n)
+        contoallarovescia(n-1)
+
+
+def stampa_n(s, n):
+    if n <= 0:
+        return
+    print(s)
+    stampa_n(s, n-1)
+
+def fai_n(j,n):
+    if i in range (n):
+        j(s,n)
+#Come esercizio, disegnate il diagramma di stack della funzione stampa_n chiamata con
+#s=Ciao e n=2. Poi, scrivete una funzione di nome fai_n che accetti come argomenti un
+#oggetto funzione e un numero n, e che chiami per n volte la funzione data
+N = 2
+s = ciao
+fai_n(stampa_n,N)
+contoallarovescia
