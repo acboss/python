5bbd57a4a51240d3776a4dd80d700d5fc6059071
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-18 13:47:53 +0200

    18 3

--- funzioneDiAckermann.py
+++ funzioneDiAckermann.py
@@ -1,18 +1,9 @@
 def A(m,n):
-    if m = 0:
-        g = n
-        n = g+1
-        return
-    if m > 0 and n = 0:
-        s = m
-        m = s-1
-        n = 1
-        return
+    if m == 0:
+        return n+1
+    if m > 0 and n == 0:
+        return A(m-1, 1)
     if m > 0 and n > 0:
-        p = m
-        n = d
-        m = p-1
-        n = A(m, d-1)
-        return
+        return A(m-1, A(m, n-1))
 
 print (A(3,4))
