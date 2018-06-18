3d1187213171a8c881cf82ad16b96d98a12f173a
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-18 14:37:09 +0200

    18 palidromo

---
+++ palindromo.py
@@ -0,0 +1,22 @@
+
+
+def prima(parola):
+    return parola[0]
+def ultima(parola):
+    return parola[-1]
+def mezzo(parola):
+    return parola[1:-1]
+
+
+
+def palidromo(parola):
+    if len(parola) <= 1:
+        return True
+    if prima(parola) != ultima(parola):
+        return False
+    return palidromo(mezzo(parola))
+
+
+print(palidromo("ottetto"))
+print(palidromo("andrea"))
+print(palidromo("otto"))
