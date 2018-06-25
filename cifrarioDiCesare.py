7fe1eb4f92b6d1fca95e8a24d4d0d91b71edd7e5
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-25 15:25:02 +0200

    1

--- cifrarioDiCesare.py
+++ cifrarioDiCesare.py
@@ -4,6 +4,28 @@ def cripta(testo,numeroCheCifra):
     n = 0
     l = 0
     if i in range (lunghezza):
-    a = parola[l]
-    lettera = ord(a)
-    cifrarata = lettera + (numeroCheCifra)
+        a = parola[l]
+        numeroLettera = ord(a)
+        cifrarata = lettera + (numeroCheCifra)
+        if cifrata == 1:
+            print ('a')
+        if cifrata == 2:
+            print ('b')
+        if cifrata == 3:
+            print ('c')
+        if cifrata == 4:
+            print ('d')
+        if cifrata == 5:
+            print ('e')
+        if cifrata == 6:
+            print ('f')
+        if cifrata == 7:
+            print
+        if cifrata == 8:
+            print
+        
+
+
+
+cripta(andrea,3)
+        
