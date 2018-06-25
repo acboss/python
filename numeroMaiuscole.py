23b6cf085ac08a3799d13f5bf7ded45b7a1d6a15
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-25 11:19:00 +0200

    3

--- numeroMaiuscole.py
+++ numeroMaiuscole.py
@@ -1,17 +1,18 @@
 def numeroMaiuscole(parola):
     str(parola)                      
-    lunghezza = (len(parola))-1
-    n = 0
-    l = -1
-    a = parola[l]
-    while l != lunghezza:
-        l+1
-        str.islower(a)
+    lunghezza = (len(parola))-1        #trova la lunghezza della parola e sottrae 1 perchè nelle parentesi quadre la lunghezza inizia a essere contata da 0 nel senso se ho un stringa ciao la lunghezza(len) è 4 e la lettere o corrispondre a ciao[3]
+    n = 0                             #numero di maiuscole
+    l = 0                             #lunghezza della parola che aumenta fino a quando non è uguale a lunghezza
+    a = parola[l]                     #a diventa la prima lettera della parola
+    while l != lunghezza:             #lunghezza della parola che aumenta fino a quando non è uguale a lunghezza
+        str.islower(a)                #controlla se la lettera è maiuscola o no
         if str.islower(a) == True:
-            return n+1 
+            return n                  #significa che  non è minuscola quindi n non aumenta il suo valore
         elif str.islower(a) == False:
-            return n
-
+            return n+1                #significa che è una maiuscolq quindi n aumenta il suo valore
+        return l+1                    #l aumenta il suo valore di 1 così il controllo della maiuscola slitta alla lettera successiva
+                                      #dopo ciò la funzione deve ripetersi ma non lo fa 
+ 
 
 
 
