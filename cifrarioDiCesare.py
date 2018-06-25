29f2d9da96b659062dddd2adee7cef51b04b37c0
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-25 12:32:01 +0200

    5

--- cifrarioDiCesare.py
+++ cifrarioDiCesare.py
@@ -1,2 +1,8 @@
 def cripta(testo,numeroCheCifra):
-    
+    str(testo)
+    lunghezza = (len(testo))
+    n = 0
+    l = 0
+    if i in range
+    a = parola[l]
+    ord(a)
--- numeroMaiuscole.py
+++ numeroMaiuscole.py
@@ -3,14 +3,15 @@ def numeroMaiuscole(parola):
     lunghezza = (len(parola))-1        #trova la lunghezza della parola e sottrae 1 perchè nelle parentesi quadre la lunghezza inizia a essere contata da 0 nel senso se ho un stringa ciao la lunghezza(len) è 4 e la lettere o corrispondre a ciao[3]
     n = 0                             #numero di maiuscole
     l = 0                             #lunghezza della parola che aumenta fino a quando non è uguale a lunghezza                              
-    for i in range (while l != lunghezza):             #lunghezza della parola che aumenta fino a quando non è uguale a lunghezza
-        str.islower(a)                #controlla se la lettera è maiuscola o no
-        if str.islower(a) == True:
+    if 0 == 0 in range (lunghezza):             #lunghezza della parola che aumenta fino a quando non è uguale a lunghezza
+        l+1
+        a = parola[l]
+        a.islower()                #controlla se la lettera è maiuscola o no
+        if a.islower() == True:
             return n                  #significa che  non è minuscola quindi n non aumenta il suo valore
-        elif str.islower(a) == False:
-            return n+1                #significa che è una maiuscolq quindi n aumenta il suo valore
-        a = parola[l]                 #a diventa la prima lettera della parola
-        return l+1                    #l aumenta il suo valore di 1 così il controllo della maiuscola slitta alla lettera successiva
+        elif a.islower() == False:
+            return n+1                #significa che è una maiuscolq quindi n aumenta il suo valore               #a diventa la prima lettera della parola
+                         #l aumenta il suo valore di 1 così il controllo della maiuscola slitta alla lettera successiva
                                       #dopo ciò la funzione deve ripetersi ma non lo fa 
  
 
