6b29bf1bea2fbfdfe12b1d3f6004f8cc7cd18809
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-27 12:49:22 +0200

    k

--- prove.py
+++ prove.py
@@ -1,12 +1,50 @@
-fin = open('words.txt')
-fin.readline() #legge una riga e va a capo se si riscrive legge la riga successiva e va a capo e così via
+#fin = open('words.txt')
+#fin.readline() #legge una riga e va a capo se si riscrive legge la riga successiva e va a capo e così via
 
 
-riga = fin.readline()
-parola = riga.strip()
-print(parola) #se si vuole togliere lo \n
+#riga = fin.readline()
+#parola = riga.strip()
+#print(parola) #se si vuole togliere lo \n
 
 #fin = open('words.txt')
 #for riga in fin:
 #    parola = riga.strip()
 #    print(parola) #stampa tutte le parole una per riga
+
+from __future__ import print_function, division
+
+
+def is_triple_double(word):
+    """Tests if a word contains three consecutive double letters.
+    
+    word: string
+
+    returns: bool
+    """
+    i = 0
+    count = 0
+    while i < len(word)-1:
+        if word[i] == word[i+1]:
+            count = count + 1
+            if count == 3:
+                return True
+            i = i + 2
+        else:
+            count = 0
+            i = i + 1
+    return False
+
+
+def find_triple_double():
+    """Reads a word list and prints words with triple double letters."""
+    fin = open('words.txt')
+    for line in fin:
+        word = line.strip()
+        if is_triple_double(word):
+            print(word)
+
+
+print('Here are all the words in the list that have')
+print('three consecutive double letters.')
+find_triple_double()
+print('')
--- senzaE.py
+++ senzaE.py
@@ -11,3 +11,26 @@ for riga in fin:
             print('')
         elif lunghezza == -1:
             print(parola)
+def senzaE(parola):
+    i = 0
+    lunghezza = (len(parola))-1
+    while i < lunghezza:
+        a = riga[lunghezza]
+        if ord(a) == 69 or ord(a) == 101:
+            return False
+            
+        else:
+            return True
+        lunghezza = lunghezza-1
+        if lunghezza == -1:
+            break
+
+def trovaLeE():
+
+    fin = open('words.txt')
+    for line in fin:
+        parola = line.strip()
+        if senzaE(parola):
+            print(parola)
+
+trovaLeE()
