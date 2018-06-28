22cdf1fe18f004869dc5d03395b5e69b67597790
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-28 17:02:11 +0200

    2

---
+++ sommaComulata.py
@@ -0,0 +1,10 @@
+def somma_comulata(numeri):
+    totale = 0
+    sommaComulata = []
+    for x in numeri:
+        totale += x
+        sommaComulata.append(totale)
+    return sommaComulata
+
+numeri = [3,2]
+print(somma_comulata(numeri))
