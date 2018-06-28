638b930053c198cb5c7d8345e3ff601fb0620564
CyborgTrooper <cyborg_trooper@msn.com>
2018-06-28 13:21:31 +0200

    tvh

--- usaSolo.py
+++ usaSolo.py
@@ -1,17 +1,23 @@
 
-def usaSolo(parola,lettere):
+def ciSono(parola,lettere):
     i = 0
     lunghezza = (len(parola))-1
     while i < lunghezza:
         a = parola[lunghezza]
         if a in lettere:
             return True
-        else:
             lunghezza = lunghezza-1
     return False
 
+def usaSolo(parola,lettere):
+    if ciSono(parola,lettere) == True:
+        if parola in lettere:
+            return True
+        else:
+            return False
 
 
-lettere = ['a','b']
+lettere = ['s']
+print(ciSono('ciao',lettere))
 print(usaSolo('ciao',lettere))
 
